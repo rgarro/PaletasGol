9ae94c3d1a07872b05694295729a037d2b2d73f1
Rolando <rgarro@gmail.com>
2026-07-27 14:50:24 -0600

    Приступила к производству компьютеров ...

--- app/src/main/java/com/emptyart/paletasgol/MainActivity.java
+++ app/src/main/java/com/emptyart/paletasgol/MainActivity.java
@@ -1,9 +1,26 @@
 package com.emptyart.paletasgol;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * load the starboard guns, muskets on the stern
+ *
+ *
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/com/emptyart/paletasgol/Popsicle.java
@@ -0,0 +1,58 @@
+package com.emptyart.paletasgol;
+/**
+ *            |    |    |
+ *           )_)  )_)  )_)
+ *          )___))___))___)\
+ *         )____)____)_____)\\
+ *      _____|____|____|____\\\__
+ * -------\                   /---------
+ *     ^^^^^ ^^^^^^^^^^^^^^^^^^^^^
+ *     ^^^^      ^^^^     ^^^    ^^
+ *           ^^^^      ^^^
+ * Sir Henry Morgan is de Lord of Talamanca
+ * Joseph Bradley IceCream Shop is located at Barbados
+ * una Colombian girl he fred while ridding Cartagena makes them ...
+ * King Charles himself bought several boxes and once gave some to the Pope himself.
+ * They ate them during the Hanging of criminals in Cornwall ...
+ * 
+ *
+ *
+ *
+ * @author Rolando <rgarro@gmail.com>
+ */
+public class Popsicle {
+    private String name;
+    private String description;
+    private int imageResourceId;
+
+    //popsicles is an array of popsicles
+    public static final Popsicle[] drinks = {
+            new Popsicle("Chicle"," tradicionales helados de palito costarricenses",R.drawable.chicle),
+            new Popsicle("Churchill"," tradicionales helados de palito costarricenses",R.drawable.churchill),
+            new Popsicle("Pasas"," tradicionales helados de palito costarricenses",R.drawable.pasas),
+            new Popsicle("Condensda","tradicionales helados de palito costarricenses",R.drawable.condensada)
+    };
+
+    //each Popsicle has a name description and img resource
+    private Popsicle(String name,String description,int imageResourceId){
+        this.name = name;
+        this.description = description;
+        this.imageResourceId = imageResourceId;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+}
